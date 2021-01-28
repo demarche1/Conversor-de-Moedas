9c03caff5736ec6fd83d56f684e04c03c391c146
Alessandro <alessandro.demarche00@gmail.com>
2021-01-28 15:11:46 -0300

    28-01-2021

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 function estimate(currency){
     let input = document.getElementById("exchenge_value")
     let value = Number(input.value)
@@ -27,108 +25,7 @@ function date(){
 }
 
 function exchenge(){
-    let select = document.getElementById("select").value
-    try{
-        if(select == "USD"){
-            estimate(select)
-        }
-        if(select == "CAD"){
-            estimate(select)
-        }
-        if(select == "GBP"){
-            estimate(select)
-        }
-        if(select == "EUR"){
-            estimate(select)
-        }
-        if(select == "JPY"){
-            estimate(select)
-        }
-        if(select == "CNY"){
-            estimate(select)
-        }
-        if(select == "AUD"){
-            estimate(select)
-        }
-        if(select == "HKD"){
-            estimate(select)
-        }
-        if(select == "ISK"){
-            estimate(select)
-        }
-        if(select == "PHP"){
-            estimate(select)
-        }
-        if(select == "DKK"){
-            estimate(select)
-        }
-        if(select == "HUF"){
-            estimate(select)
-        }
-        if(select == "CZK"){
-            estimate(select)
-        }
-        if(select == "RON"){
-            estimate(select)
-        }
-        if(select == "SEK"){
-            estimate(select)
-        }
-        if(select == "IDR"){
-            estimate(select)
-        }
-        if(select == "INR"){
-            estimate(select)
-        }
-        if(select == "RUB"){
-            estimate(select)
-        }
-        if(select == "HRK"){
-            estimate(select)
-        }
-        if(select == "THB"){
-            estimate(select)
-        }
-        if(select == "CHF"){
-            estimate(select)
-        }
-        if(select == "MYR"){
-            estimate(select)
-        }
-        if(select == "BGN"){
-            estimate(select)
-        }
-        if(select == "TRY"){
-            estimate(select)
-        }
-        if(select == "NOK"){
-            estimate(select)
-        }
-        if(select == "NZD"){
-            estimate(select)
-        }
-        if(select == "ZAR"){
-            estimate(select)
-        }
-        if(select == "MXN"){
-            estimate(select)
-        }
-        if(select == "SGD"){
-            estimate(select)
-        }
-        if(select == "ILS"){
-            estimate(select)
-        }
-        if(select == "KRW"){
-            estimate(select)
-        }
-        if(select == "PLN"){
-            estimate(select)
-        }
-    }
-    catch(err){
-        alert("Aconteceu algum erro, desculpe pelo o incoveniênte")
-    }
+    estimate(document.getElementById("select").value)
 }
 
 
